61410242d8130101a778c1d268a2b6446d14538d
Romain Boussot <rboussot@gmail.com>
2016-08-22 15:58:29 +0200

    table theme created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822135333) do
+ActiveRecord::Schema.define(version: 20160822135457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,19 @@ ActiveRecord::Schema.define(version: 20160822135333) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "courses", force: :cascade do |t|
+    t.integer  "author_id"
+    t.string   "title"
+    t.integer  "date"
+    t.text     "description"
+    t.integer  "category_id"
+    t.boolean  "visible"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["author_id"], name: "index_courses_on_author_id", using: :btree
+    t.index ["category_id"], name: "index_courses_on_category_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -49,4 +62,6 @@ ActiveRecord::Schema.define(version: 20160822135333) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "courses", "authors"
+  add_foreign_key "courses", "categories"
 end
