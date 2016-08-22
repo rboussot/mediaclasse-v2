9079c71c3a1cb2c9bf759b79a49af20887ca4496
Romain Boussot <rboussot@gmail.com>
2016-08-22 16:00:16 +0200

    Theme table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822135457) do
+ActiveRecord::Schema.define(version: 20160822135802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20160822135457) do
     t.datetime "updated_at",  null: false
     t.index ["author_id"], name: "index_courses_on_author_id", using: :btree
     t.index ["category_id"], name: "index_courses_on_category_id", using: :btree
+  end
+
+  create_table "themes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
