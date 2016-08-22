c65c58beda2378c9a385226a86b4332971a93d19
Romain Boussot <rboussot@gmail.com>
2016-08-22 15:52:40 +0200

    author table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822125002) do
+ActiveRecord::Schema.define(version: 20160822134930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "lastname"
+    t.string   "firstname"
+    t.string   "pseudo"
+    t.integer  "birth"
+    t.integer  "death"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
