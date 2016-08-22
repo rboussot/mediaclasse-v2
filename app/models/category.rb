53dc17d238829763210df21de6e39fb51722e8fa
Guillaume Laine <guillaumelaine2.0@gmail.com>
2016-08-22 16:51:09 +0200

    added validations on category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
 
-  validates :address, presence: true uniqueness:true
+  validates :name, presence:true
+  validate :name, uniqueness:true
+  validates :description, presence: true
 end
