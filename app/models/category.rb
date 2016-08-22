72015bfbe830fcdfbf91639351c5697026d09c21
Guillaume Laine <guillaumelaine2.0@gmail.com>
2016-08-22 16:55:11 +0200

    created db and migrated

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
 
   validates :name, presence:true
-  validate :name, uniqueness:true
+  validates :name, uniqueness:true
   validates :description, presence: true
 end
