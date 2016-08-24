ceeadb759aaa97b66f09f3a066ed832da260e14c
Romain Boussot <rboussot@gmail.com>
2016-08-24 18:09:23 +0200

    attachinary on all models

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -4,4 +4,21 @@ class Channel < ApplicationRecord
   validates :name, presence: true, uniqueness: :true
   validates :description, presence: true
   validates :email, presence: true
+  has_attachment :picture
+
+  rails_admin do
+    edit do
+      field :picture do
+        partial "upload_pictures"
+      end
+      field :user_id
+      field :category_id
+      field :name
+      field :description
+      field :email
+      field :link
+      field :visible
+    end
+  end
+
 end
