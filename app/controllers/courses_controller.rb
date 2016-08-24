96e11a41585d018f036eec42ebadb342bdcaba76
Guillaume Laine <guillaumelaine2.0@gmail.com>
2016-08-24 10:48:33 +0200

    mods in controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
 
   def litterature
     # attention a modifier l'id
-    @litterature = Course.where(category_id: 1)
+    @litterature = Course.all
     # @litterature = Course.where(category: "litterature")
     # Course n'a pas de category juste un category_id
     # seules les categories ont un name
@@ -10,7 +10,7 @@ class CoursesController < ApplicationController
 
   def technique
     # attention a modifier l'id
-    @technique = Course.where(category_id: 4)
+    # @technique = Course.where(category_id: 4)
   end
 
 end
