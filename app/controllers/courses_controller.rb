7a136da6677a9eb31455797204fc649f4f8432c2
Guillaume Laine <guillaumelaine2.0@gmail.com>
2016-08-24 11:39:14 +0200

    refacto controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,10 @@
 class CoursesController < ApplicationController
 
   def litterature
-    # attention a modifier l'id
     @litterature = Course.where(tag:"litterature")
-    # @litterature = Course.
-    # Course n'a pas de category juste un category_id
-    # seules les categories ont un name
   end
 
   def technique
-    # attention a modifier l'id
     @technique = Course.where(tag:"technique")
   end
 
