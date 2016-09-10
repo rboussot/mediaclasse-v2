71a48fb25c4b0a5943a11f93d692227a2378507a
Romain Boussot <rboussot@gmail.com>
2016-09-10 12:28:16 +0200

    default mailer host www.mediaclasse.fr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://mediaclasse-v2.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "http://www.mediaclasse.fr" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
