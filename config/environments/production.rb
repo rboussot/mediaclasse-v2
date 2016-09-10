221122a3d37eb7ad1c0b0fa3b73bf84dc449615b
Romain Boussot <rboussot@gmail.com>
2016-09-10 12:24:04 +0200

    default url mailer at herokuapp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://mediaclass-v2.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "http://mediaclasse-v2.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
