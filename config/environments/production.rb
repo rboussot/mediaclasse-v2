c0f145997ca67a40332ca50e31e840dac4db972a
Romain Boussot <rboussot@gmail.com>
2016-09-10 15:24:42 +0200

    mailer avec gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mediaclasse-v2_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  # config.action_mailer.smtp_settings =
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
