a55b183e23eac5b01594e6a30c2b1eb34ebb52ce
Romain Boussot <rboussot@gmail.com>
2017-11-20 07:28:37 +0100

    afficher les 5 derniers messages du livre d'or sur la page d'accueil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,7 +25,7 @@ class PagesController < ApplicationController
   private
 
   def find_super_comments
-    @super_comments = policy_scope(Comment).includes(user: :picture_files).where(super: true).shuffle.last(5)
+    @super_comments = policy_scope(Comment).includes(user: :picture_files).where(super: true).order(created_at: :desc).last(5)
   end
 
 end
