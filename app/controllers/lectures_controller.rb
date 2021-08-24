69edce599726490833e54ff4b197e1808e4e94f7
Romain Boussot <rboussot@gmail.com>
2021-08-24 15:23:15 +0200

    meta et balises alt sur la page de lecture

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -8,8 +8,10 @@ class LecturesController < ApplicationController
     @lectures = @lecture_course.lectures.order('updated_at ASC').where(visible: true)
     @documents = Document.where(lecture_id: params[:id])
     skip_authorization
+    @meta_title = @lecture_course.title+" de "+@lecture_author+" : "+@lecture.title+" (Explications et commentaires détaillés)"
     @description_for_subscribers = @lecture.description.gsub("\n", "<br/>").html_safe
     @description_for_free = @lecture.description.gsub("\n", "<br/>").split("<h")[0..4].join('<h').to_s.html_safe
+    @description_title = @lecture.description.gsub("\n", "<br/>").split("<br/>")[0..1].join('<br/>').to_s.html_safe
   end
 
 end
