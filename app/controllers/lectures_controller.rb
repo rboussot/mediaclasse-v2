ffad8fbbed6f9668ddebc4ab977d0ffce266992d
Romain Boussot <rboussot@gmail.com>
2021-08-24 14:34:46 +0200

    style du contenu gratuit et payant

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -8,6 +8,8 @@ class LecturesController < ApplicationController
     @lectures = @lecture_course.lectures.order('updated_at ASC').where(visible: true)
     @documents = Document.where(lecture_id: params[:id])
     skip_authorization
+    @description_for_subscribers = @lecture.description.gsub("\n", "<br/>").html_safe
+    @description_for_free = @lecture.description.gsub("\n", "<br/>").split("<h")[0..4].join('<h').to_s.html_safe
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,200 +10,203 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190911055127) do
+ActiveRecord::Schema.define(version: 2019_09_11_055127) do
 
-  create_table "attachinary_files", force: :cascade do |t|
-    t.string   "attachinariable_type"
-    t.integer  "attachinariable_id"
-    t.string   "scope"
-    t.string   "public_id"
-    t.string   "version"
-    t.integer  "width"
-    t.integer  "height"
-    t.string   "format"
-    t.string   "resource_type"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attachinary_files", id: :serial, force: :cascade do |t|
+    t.string "attachinariable_type"
+    t.integer "attachinariable_id"
+    t.string "scope"
+    t.string "public_id"
+    t.string "version"
+    t.integer "width"
+    t.integer "height"
+    t.string "format"
+    t.string "resource_type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
+    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
   end
 
-  create_table "authors", force: :cascade do |t|
-    t.string   "lastname"
-    t.string   "firstname"
-    t.string   "pseudo"
-    t.integer  "birth"
-    t.integer  "death"
-    t.string   "keywords"
-    t.string   "picture"
+  create_table "authors", id: :serial, force: :cascade do |t|
+    t.string "lastname"
+    t.string "firstname"
+    t.string "pseudo"
+    t.integer "birth"
+    t.integer "death"
+    t.string "keywords"
+    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "tag"
-    t.string   "video"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "channels", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "category_id"
-    t.string   "name"
-    t.text     "description"
-    t.string   "email"
-    t.string   "link"
-    t.boolean  "visible",     default: false, null: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.index ["category_id"], name: "index_channels_on_category_id", using: :btree
-    t.index ["user_id"], name: "index_channels_on_user_id", using: :btree
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.integer  "lecture_id"
-    t.integer  "user_id"
-    t.text     "content"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "super",      default: false, null: false
-    t.boolean  "visible",    default: true,  null: false
-    t.index ["lecture_id"], name: "index_comments_on_lecture_id", using: :btree
-    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
-  end
-
-  create_table "courses", force: :cascade do |t|
-    t.integer  "author_id"
-    t.string   "title"
-    t.integer  "date"
-    t.text     "description"
-    t.integer  "category_id"
-    t.boolean  "visible",     default: true, null: false
-    t.text     "keywords"
-    t.string   "picture"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.index ["author_id"], name: "index_courses_on_author_id", using: :btree
-    t.index ["category_id"], name: "index_courses_on_category_id", using: :btree
-  end
-
-  create_table "courses_themes", force: :cascade do |t|
-    t.integer  "course_id"
-    t.integer  "theme_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["course_id"], name: "index_courses_themes_on_course_id", using: :btree
-    t.index ["theme_id"], name: "index_courses_themes_on_theme_id", using: :btree
-  end
-
-  create_table "documents", force: :cascade do |t|
-    t.integer  "lecture_id"
-    t.string   "title"
-    t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["lecture_id"], name: "index_documents_on_lecture_id", using: :btree
-  end
-
-  create_table "lectures", force: :cascade do |t|
-    t.integer  "course_id"
-    t.string   "title"
-    t.string   "video"
-    t.text     "description"
-    t.string   "picture"
-    t.boolean  "payment",     default: false, null: false
-    t.boolean  "visible",     default: true,  null: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.index ["course_id"], name: "index_lectures_on_course_id", using: :btree
-  end
-
-  create_table "levels", force: :cascade do |t|
-    t.string   "name"
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "tag"
+    t.string "video"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "plans", force: :cascade do |t|
-    t.string   "plan_name"
-    t.float    "price"
-    t.string   "css_class"
-    t.string   "title"
-    t.text     "description"
-    t.boolean  "visible"
-    t.string   "payment"
-    t.text     "message"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "subcomments", force: :cascade do |t|
-    t.integer  "comment_id"
-    t.integer  "user_id"
-    t.text     "content"
+  create_table "channels", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
+    t.string "name"
+    t.text "description"
+    t.string "email"
+    t.string "link"
+    t.boolean "visible", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["comment_id"], name: "index_subcomments_on_comment_id", using: :btree
-    t.index ["user_id"], name: "index_subcomments_on_user_id", using: :btree
+    t.index ["category_id"], name: "index_channels_on_category_id"
+    t.index ["user_id"], name: "index_channels_on_user_id"
   end
 
-  create_table "themes", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "level_id"
-    t.index ["level_id"], name: "index_themes_on_level_id", using: :btree
+  create_table "comments", id: :serial, force: :cascade do |t|
+    t.integer "lecture_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "super", default: false, null: false
+    t.boolean "visible", default: true, null: false
+    t.index ["lecture_id"], name: "index_comments_on_lecture_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "courses", id: :serial, force: :cascade do |t|
+    t.integer "author_id"
+    t.string "title"
+    t.integer "date"
+    t.text "description"
+    t.integer "category_id"
+    t.boolean "visible", default: true, null: false
+    t.text "keywords"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_courses_on_author_id"
+    t.index ["category_id"], name: "index_courses_on_category_id"
+  end
+
+  create_table "courses_themes", id: :serial, force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "theme_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_courses_themes_on_course_id"
+    t.index ["theme_id"], name: "index_courses_themes_on_theme_id"
+  end
+
+  create_table "documents", id: :serial, force: :cascade do |t|
+    t.integer "lecture_id"
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lecture_id"], name: "index_documents_on_lecture_id"
+  end
+
+  create_table "lectures", id: :serial, force: :cascade do |t|
+    t.integer "course_id"
+    t.string "title"
+    t.string "video"
+    t.text "description"
+    t.string "picture"
+    t.boolean "payment", default: false, null: false
+    t.boolean "visible", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lectures_on_course_id"
+  end
+
+  create_table "levels", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", id: :serial, force: :cascade do |t|
+    t.string "plan_name"
+    t.float "price"
+    t.string "css_class"
+    t.string "title"
+    t.text "description"
+    t.boolean "visible"
+    t.string "payment"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subcomments", id: :serial, force: :cascade do |t|
+    t.integer "comment_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_subcomments_on_comment_id"
+    t.index ["user_id"], name: "index_subcomments_on_user_id"
+  end
+
+  create_table "themes", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "level_id"
+    t.index ["level_id"], name: "index_themes_on_level_id"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.integer  "level_id"
-    t.string   "firstname"
-    t.string   "lastname"
-    t.string   "adress"
-    t.string   "phone"
-    t.date     "birth"
-    t.boolean  "visible",                default: true,  null: false
-    t.boolean  "admin",                  default: false
-    t.string   "stripe_customer_id"
-    t.string   "plan"
-    t.date     "paydate"
-    t.boolean  "deleted",                default: false, null: false
-    t.boolean  "collective",             default: false, null: false
-    t.string   "courses_ids"
-    t.float    "pricing",                default: 0.0
-    t.date     "expire"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["level_id"], name: "index_users_on_level_id", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "level_id"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "adress"
+    t.string "phone"
+    t.date "birth"
+    t.boolean "visible", default: true, null: false
+    t.boolean "admin", default: false
+    t.string "stripe_customer_id"
+    t.string "plan"
+    t.date "paydate"
+    t.boolean "deleted", default: false, null: false
+    t.boolean "collective", default: false, null: false
+    t.string "courses_ids"
+    t.float "pricing", default: 0.0
+    t.date "expire"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["level_id"], name: "index_users_on_level_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "votes", force: :cascade do |t|
-    t.string   "votable_type"
-    t.integer  "votable_id"
-    t.string   "voter_type"
-    t.integer  "voter_id"
-    t.boolean  "vote_flag"
-    t.string   "vote_scope"
-    t.integer  "vote_weight"
+  create_table "votes", id: :serial, force: :cascade do |t|
+    t.string "votable_type"
+    t.integer "votable_id"
+    t.string "voter_type"
+    t.integer "voter_id"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
-    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
 
   add_foreign_key "channels", "categories"
