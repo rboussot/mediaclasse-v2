0e59119d98c5ddab1c30b9ab3ffaf4e2f2e76b82
Romain Boussot <rboussot@gmail.com>
2021-08-25 09:12:05 +0200

    amélioration des citations et contenus payants

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -9,8 +9,8 @@ class LecturesController < ApplicationController
     @documents = Document.where(lecture_id: params[:id])
     skip_authorization
     @meta_title = @lecture_course.title+" de "+@lecture_author+" : "+@lecture.title+" (Explications et commentaires détaillés)"
-    @description_for_subscribers = @lecture.description.gsub("\n", "<br/>").html_safe
-    @description_for_free = @lecture.description.gsub("\n", "<br/>").split("<h")[0..4].join('<h').to_s.html_safe
+    @description_for_subscribers = @lecture.description.gsub("\n", "<br/>").split("[/]").join(" ").to_s.html_safe
+    @description_for_free = @lecture.description.gsub("\n", "<br/>").split("[/]")[0..0].join(" ").to_s.html_safe
     @description_title = @lecture.description.gsub("\n", "<br/>").split("<br/>")[0..1].join('<br/>').to_s.html_safe
   end
 
