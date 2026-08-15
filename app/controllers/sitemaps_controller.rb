7cd1216269f85ce6bfad2a6fc3dc188651711f1a
Romain Boussot <rboussot@gmail.com>
2026-08-16 00:20:02 +0200

    corrections pour la sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,6 +1,7 @@
 class SitemapsController < ApplicationController
   skip_before_action :authenticate_user!, raise: false
-  
+  skip_authorization if respond_to?(:skip_authorization) # Exempte Pundit si présent
+
   def index
     # On prend uniquement les leçons visibles pour ne pas envoyer Google sur des pages masquées
     @lectures = Lecture.where(visible: true)
